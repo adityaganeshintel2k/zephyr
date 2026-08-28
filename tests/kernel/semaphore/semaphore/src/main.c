cf3d5dd74d2def6ceea898dbb2042e42f3b05e00
Aditya Ganesh <aditya.ganesh@intel.com>
2026-08-28 11:49:37 -0700

    tests: kernel: semaphore: semaphore: main.c
    Changed TOTAL_THREADS_WAITING to unsigned int as it is not being negative anywhere and does not look like a variable that will be negative. This is done to remove any warnings of different type sign compare
    
    Signed-off-by: Aditya Ganesh <aditya.ganesh@intel.com>

--- tests/kernel/semaphore/semaphore/src/main.c
+++ tests/kernel/semaphore/semaphore/src/main.c
@@ -26,7 +26,7 @@
 
 #define SEM_TIMEOUT (K_MSEC(100))
 #define STACK_SIZE (512 + CONFIG_TEST_EXTRA_STACK_SIZE)
-#define TOTAL_THREADS_WAITING (5)
+#define TOTAL_THREADS_WAITING (5U)
 
 #define SEC2MS(s) ((s) * 1000)
 #define QSEC2MS(s) ((s) * 250)
@@ -515,7 +515,7 @@ ZTEST_USER(semaphore, test_sem_count_get)
 	expect_k_sem_count_get_nomsg(&msg_sema, SEM_INIT_VAL + 1);
 	expect_k_sem_take_nomsg(&msg_sema, K_FOREVER, 0);
 	/**TESTPOINT: sem count get after take*/
-	for (unsigned int i = 0; i < SEM_MAX_VAL; i++) {
+	for (unsigned int i = 0U; i < SEM_MAX_VAL; i++) {
 		expect_k_sem_count_get_nomsg(&msg_sema, SEM_INIT_VAL + i);
 		k_sem_give(&msg_sema);
 	}
@@ -952,7 +952,7 @@ ZTEST_USER(semaphore, test_k_sem_correct_count_limit)
 	/* Give the semaphore by a thread and verify the semaphore's
 	 * count is as expected
 	 */
-	for (unsigned int i = 1; i <= SEM_MAX_VAL; i++) {
+	for (unsigned int i = 1U; i <= SEM_MAX_VAL; i++) {
 		k_sem_give(&simple_sem);
 		expect_k_sem_count_get_nomsg(&simple_sem, i);
 	}
@@ -961,7 +961,7 @@ ZTEST_USER(semaphore, test_k_sem_correct_count_limit)
 	 * continue to run k_sem_give,
 	 * the count of simple_sem will not increase anymore
 	 */
-	for (unsigned int i = 0; i < 5; i++) {
+	for (unsigned int i = 0U; i < 5; i++) {
 		k_sem_give(&simple_sem);
 		expect_k_sem_count_get_nomsg(&simple_sem, SEM_MAX_VAL);
 	}
@@ -1009,7 +1009,7 @@ ZTEST(semaphore, test_sem_give_take_from_isr)
 	expect_k_sem_count_get_nomsg(&simple_sem, 0U);
 
 	/* give semaphore from an isr and do a check for the count */
-	for (unsigned int i = 0; i < SEM_MAX_VAL; i++) {
+	for (unsigned int i = 0U; i < SEM_MAX_VAL; i++) {
 		sem_give_from_isr(&simple_sem);
 		expect_k_sem_count_get_nomsg(&simple_sem, i + 1);
 	}
@@ -1393,7 +1393,7 @@ ZTEST(semaphore, test_sem_multi_take_timeout_diff_sem)
 	/* Multiple threads timeout on different semaphores and the sequence
 	 * in which it times out is pushed into a pipe and checked later on.
 	 */
-	for (int i = 0; i < TOTAL_THREADS_WAITING; i++) {
+	for (unsigned int i = 0U; i < TOTAL_THREADS_WAITING; i++) {
 		k_thread_create(&multiple_tid[i],
 				multiple_stack[i], STACK_SIZE,
 				sem_multi_take_timeout_diff_sem_helper,
@@ -1402,7 +1402,7 @@ ZTEST(semaphore, test_sem_multi_take_timeout_diff_sem)
 				K_PRIO_PREEMPT(1), 0, K_NO_WAIT);
 	}
 
-	for (int i = 0; i < TOTAL_THREADS_WAITING; i++) {
+	for (unsigned int i = 0U; i < TOTAL_THREADS_WAITING; i++) {
 		rc = k_pipe_read(&timeout_info_pipe, (uint8_t *)&retrieved_info,
 			sizeof(struct timeout_info), K_FOREVER);
 		zassert_true(rc == sizeof(struct timeout_info),
@@ -1411,7 +1411,7 @@ ZTEST(semaphore, test_sem_multi_take_timeout_diff_sem)
 		zassert_true(retrieved_info.timeout == SEC2MS(i + 1),
 			     "timeout did not occur properly");
 	}
-	for (int i = 0; i < TOTAL_THREADS_WAITING; i++) {
+	for (unsigned int i = 0U; i < TOTAL_THREADS_WAITING; i++) {
 		k_thread_join(&multiple_tid[i], K_FOREVER);
 	}
 }
